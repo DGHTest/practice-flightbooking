0ed27c0166f9082d57d229bcc2c4db1228b913a7
Diego <diegolin280203@hotmail.com>
2022-09-26 18:27:16 -0500

    Second commit

---
+++ src/main/java/com/practice/flightbooking/PracticeFlightbookingApplication.java
@@ -0,0 +1,13 @@
+package com.practice.flightbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeFlightbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeFlightbookingApplication.class, args);
+	}
+
+}
